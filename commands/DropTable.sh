f9f216c4618f3ce7dcf576760b970354aee223b5
sohybnaim <58359993+sohybnaim@users.noreply.github.com>
2022-01-22 21:13:18 +0200

    Create DropTable.sh

---
+++ commands/DropTable.sh
@@ -0,0 +1,21 @@
+    echo "Enter the table you want to drop:"
+    read TableName
+    
+    flag_d=0
+    
+    dropT_arr=(`ls`)
+    for i in $(seq ${#dropT_arr[@]})
+    do  
+        if [ "${dropT_arr[i-1]}" = "$TableName" ]
+        then
+            flag_d=1
+        fi
+    done
+
+    if [ $flag_d -eq 1 ]
+    then
+        rm $TableName
+        echo "$TableName table is dropped"
+    else
+        echo "This table name doesn't exist"
+    fi
