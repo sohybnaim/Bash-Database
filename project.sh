5cbc0b12812ac2c5542a0f83819ee0a77677492e
sohybnaim <58359993+sohybnaim@users.noreply.github.com>
2022-01-22 22:47:40 +0200

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,3 +1,26 @@
+#!/bin/bash
+#  function CreateTable {
+#     echo "Enter the Table Name:"
+#     read TableName
+#     flag=0
+
+#     createT_arr=(`ls`)
+#     for i in $(seq 1 ${#createT_arr[@]})
+#     do  
+#         if [ "${createT_arr[i-1]}" = "$TableName" ]
+#         then 
+#             echo "This table name already exists"
+#             flag=1
+#         fi
+#     done
+
+#     if [ $flag -ne 1 ]
+#     then
+#         touch $TableName
+#         echo "$TableName table is created successfully"
+#     fi
+# }
+
 echo "~~~ MENU ~~~"
 echo "Please enter the number of choice:"
 options=("Create DataBase" "List" "Connect To DB" "Drop DB" "Exit")
@@ -5,16 +28,16 @@ select val in "${options[@]}"
 do
     case $val in
         "Create DataBase")
-           clear ; . commands/CreateDatabase.sh ; . backTOmenu.sh
+           clear ; . CreateDatabase.sh ; . backTOmenu.sh
             ;;
         "List")
-           clear ; . commands/ListDataBase.sh ; . backTOmenu.sh
+           clear ; . ListDataBase.sh ; . backTOmenu.sh
             ;;
         "Connect To DB")
-            clear ; . commands/ConnectDatabase.sh ; . backTOmenu.sh
+            clear ; . ConnectDatabase.sh ; . backTOmenu.sh
            ;;
         "Drop DB")
-            clear ; . commands/DeletDatabase.sh ; . backTOmenu.sh
+            clear ; . DeletDatabase.sh ; . backTOmenu.sh
            ;;  
         "Exit")
             echo "Goodbye 😘" ; exit
