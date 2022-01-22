f2695790e4772c38ae0ec0df1a239a9f729266b6
sohybnaim <58359993+sohybnaim@users.noreply.github.com>
2022-01-22 21:06:42 +0200

    Create delet_DB.sh

---
+++ commands/delet_DB.sh
@@ -0,0 +1,14 @@
+function DropDatabase {
+    echo "Enter the database you want to drop:"
+    read DB
+    
+    findRes=`find -name $DB`
+
+    if [ "./$DB" = "$findRes" ] 
+        then
+        echo "print yes or no to confirm droping"
+            rm -Ir $DB  
+    else
+        echo "$DB database doesn't exist"
+    fi
+}
