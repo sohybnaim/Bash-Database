fae93c8aca3fc3ff126d014381e7646732862e86
sohybnaim <58359993+sohybnaim@users.noreply.github.com>
2022-01-22 22:47:25 +0200

    Update ConnectDatabase.sh

--- commands/ConnectDatabase.sh
+++ commands/ConnectDatabase.sh
@@ -3,10 +3,10 @@
 function ConnectMenu {
     echo "Successfully connected to $DatabaseConnect database"
     echo "Connect Menu:"
-    select choice in "Create Table" "List Tables" "Drop Table" "Insert into Table" "Select From Table" "Delete From Table" "Go to Main Menu"
+        select choice in "Create Table" "List Tables" "Drop Table" "Insert into Table" "Select From Table" "Delete From Table" "Go to Main Menu"
     do    
     case $choice in
-        "Create Table" ) CreateTable ;;
+        "Create Table" ) CreateTable.sh ;;
         "List Tables" ) listTables ;;
         "Drop Table" ) DropTable ;;
         "Insert into Table" ) InsertIntoTable;;
