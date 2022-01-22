9d6a028205545b18947fcc884bb76a9b9e8e197c
sohybnaim <58359993+sohybnaim@users.noreply.github.com>
2022-01-22 21:25:03 +0200

    Create CreateDatabase.sh

---
+++ commands/CreateDatabase.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function  {
+    echo "Enter the Database Name:"
+    read databaseName
+
+    findRes=`find -name $databaseName 2>>/dev/null`
+
+    if [ "./$databaseName" = "$findRes" ]
+        then 
+        echo "This Database name already exists"
+    else
+        mkdir $databaseName
+        echo "$databaseName database is created successfully" 
+    fi
+}
