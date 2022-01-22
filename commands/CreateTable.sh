d559458a4c2d9cd0531a7a933b4f611b36ca613a
sohybnaim <58359993+sohybnaim@users.noreply.github.com>
2022-01-22 21:17:14 +0200

    Create CreateTable.sh

---
+++ commands/CreateTable.sh
@@ -0,0 +1,19 @@
+    echo "Enter the Table Name:"
+    read TableName
+    flag=0
+
+    createT_arr=(`ls`)
+    for i in $(seq 1 ${#createT_arr[@]})
+    do  
+        if [ "${createT_arr[i-1]}" = "$TableName" ]
+        then 
+            echo "This table name already exists"
+            flag=1
+        fi
+    done
+
+    if [ $flag -ne 1 ]
+    then
+        touch $TableName
+        echo "$TableName table is created successfully"
+    fi
